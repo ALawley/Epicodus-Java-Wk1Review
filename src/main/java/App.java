0610288c54d7bc48076eb41babbf4ba777a07b20
Anna Lawley <alawley1@gmail.com>
2016-02-12 08:40:02 -0800

    build simple UI

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,11 +5,29 @@ import spark.template.velocity.VelocityTemplateEngine;
 import static spark.Spark.*;
 
 public class App {
-  public static void main(String[] args) {}
+  public static void main(String[] args) {
+    String layout = "templates/layout.vtl";
+
+    get("/", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/home.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/puzzle", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/puzzle.vtl");
+
+      String userInput = request.queryParams("puzzle-form");
+      String wordPuzzle = puzzleBuild(userInput);
+
+      model.put("userPuzzle", wordPuzzle);
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+  }
 
   public static String puzzleBuild(String inputText) {
-    String outputText = inputText.replaceAll("[aeiouAEIOU]", "-");
-    return outputText;
+    return inputText.replaceAll("[aeiouAEIOU]", "-");
   }
 
 }
