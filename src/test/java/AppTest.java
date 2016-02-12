05cf340f68ea9f49e2cca898f045b0c4339811e5
Anna Lawley <alawley1@gmail.com>
2016-02-12 08:23:52 -0800

    write additional unit tests

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -4,8 +4,23 @@ import static org.junit.Assert.*;
 public class AppTest {
 
   @Test
-  public void puzzleBuild_enterAStringAndReceiveItBackWithVowelsAsDashes() {
+  public void puzzleBuild_enterAWordAndReceiveItBackWithVowelsAsDashes() {
     App testPuzzle = new App();
     assertEquals("h-ll-", testPuzzle.puzzleBuild("hello"));
+  }
+  @Test
+  public void puzzleBuild_enterASentenceAndReceiveItBackWithVowelsAsDashes() {
+    App testPuzzle = new App();
+    assertEquals("h-ll- h-w -r- y-- my n-m- -s -nn-", testPuzzle.puzzleBuild("hello how are you my name is anna"));
+  }
+  @Test
+  public void puzzleBuild_preservePunctuation() {
+    App testPuzzle = new App();
+    assertEquals("h-ll-, h-w -r- y--? my n-m- -s -nn-.", testPuzzle.puzzleBuild("hello, how are you? my name is anna."));
+  }
+  @Test
+  public void puzzleBuild_preserveCapitalization() {
+    App testPuzzle = new App();
+    assertEquals("H-ll- h-w -r- y-- my n-m- -s -nn-", testPuzzle.puzzleBuild("Hello how are you my name is Anna"));
   }
 }
